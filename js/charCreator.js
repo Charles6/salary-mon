db282200b6c5db23124634d3beb2f98fcba36fb6
Ruth <hugthebluebow@gmail.com>
2017-11-28 17:10:37 -0800

    Character creation
    Added a new file dedicated to character creation. Included a descriptive comment with goals

---
+++ js/charCreator.js
@@ -0,0 +1,9 @@
+//this file handles Character Creation
+/* 
+	Tasks:
+	 +character creation menu on start
+	 +display name and avatar
+	 +display selected avatar monster 
+	 +display selected avatar costume
+	 +display selected avatar accessories
+*/
